1838996f390c58bfd9401b84d95bf8716c360707
hitesh.sarsava <hitesh.sarsava@sufalamtech.com>
2021-06-25 12:59:43 +0530

    Updated demo url for production.

--- SadadSDK/app/src/main/java/com/sdkdemo/api/ServerConfig.java
+++ SadadSDK/app/src/main/java/com/sdkdemo/api/ServerConfig.java
@@ -9,5 +9,5 @@ public class ServerConfig {
 //    public static final String SERVER_SANDBOX_URL = "http://sadad.de/sadadsdkclientsandbox/"; // Sandbox SERVER_SANDBOX_URL
 
 
-    public static final String SERVER_LIVE_URL = "http://172.104.180.157/sadadSDKTestConfig/";//Live
+    public static final String SERVER_LIVE_URL = "http://paylna.me/sadadSDKLiveConfig/";//Live
 }
